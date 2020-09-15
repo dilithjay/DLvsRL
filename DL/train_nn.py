f6c893ce86d022b1a9e30deab201170249152e32
dilithjay <54039395+dilithjay@users.noreply.github.com>
2020-09-15 14:46:40 +0530

    Update train_nn.py

--- DL/train_nn.py
+++ DL/train_nn.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
+
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=[3]),
     tf.keras.layers.Dense(64, activation='relu'),
@@ -14,10 +16,15 @@ with open("data.txt", "r") as file:
 xs, ys = [], []
 for i in rows:
     row = list(map(float, i.split()))
-    print(row)
     xs.append(row[:3])
     ys.append(row[-1])
 
 model.fit(xs, ys, epochs=100)
-
 model.save_weights('model.h5')
+
+loss = history.history['loss']
+epochs = range(len(loss))
+
+plt.plot(epochs, loss, 'r', label='loss')
+plt.legend(loc=0)
+plt.show()
